5886edf431ee5078c9b282c0eaa740a13c8120cf
Ebony Martin <ebjoy93@gmail.com>
2019-04-01 20:02:40 -0400

    updated csv and sql file

--- pokemonDB.sql
+++ pokemonDB.sql
@@ -1,14 +1,10 @@
 USE pokemon;
 
-SELECT * FROM information_schema.columns WHERE table_name = 'pokemon_stats';
-
-SHOW CREATE TABLE pokemon_stats;
-
 CREATE TABLE `stats` (
 `Number` int(11) DEFAULT NULL,
-`Name` text,
-`Type_1` text,
-`Type_2` text,
+`Name` text DEFAULT NULL,
+`Type_1` text DEFAULT NULL,
+`Type_2` text DEFAULT NULL,
 `Total` int(11) DEFAULT NULL,
 `HP` int(11) DEFAULT NULL,
 `Attack` int(11) DEFAULT NULL,
@@ -17,17 +13,17 @@ CREATE TABLE `stats` (
 `Sp_Def` int(11) DEFAULT NULL,
 `Speed` int(11) DEFAULT NULL,
 `Generation` int(11) DEFAULT NULL,
-`isLegendary` text,
-`Color` text,
-`hasGender` text,
+`isLegendary` text DEFAULT NULL,
+`Color` text DEFAULT NULL,
+`hasGender` text DEFAULT NULL,
 `Pr_Male` double DEFAULT NULL,
-`Egg_Group_1` text,
-`Egg_Group_2` text,
-`hasMegaEvolution` text,
+`Egg_Group_1` text DEFAULT NULL,
+`Egg_Group_2` text DEFAULT NULL,
+`hasMegaEvolution` text DEFAULT NULL,
 `Height_m` double DEFAULT NULL,
 `Weight_kg` double DEFAULT NULL,
 `Catch_Rate` int(11) DEFAULT NULL,
-`Body_Style` text);
+`Body_Style` text DEFAULT NULL);
 
-DROP TABLE pokemon_stats;
+
 
