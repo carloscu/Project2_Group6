8631cd19f081018495fefb31d4389827c2486c85
Enola Gay <betsyshane@gmail.com>
2019-04-08 19:04:17 -0400

    pushing sat work

---
+++ BSmodel/static/js/appbs.js
@@ -0,0 +1,22 @@
+// Plot the default route once the page loads
+var defaultURL = "/type_total";
+d3.json(defaultURL).then(function(data) {
+  var data = [data];
+  var layout = { margin: { t: 30, b: 100 } };
+  Plotly.plot("bar", data, layout);
+});
+
+// Update the plot with new data
+function updatePlotly(newdata) {
+  Plotly.restyle("x", [newdata.x]);
+  Plotly.restyle( "y", [newdata.y]);
+}
+
+// Get new data whenever the dropdown selection changes
+function getData(route) {
+  console.log(route);
+  d3.json(`/${route}`).then(function(data) {
+    console.log("newdata", data);
+    updatePlotly(data);
+  });
+}
